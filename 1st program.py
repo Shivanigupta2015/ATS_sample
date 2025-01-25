ad1cb5166457cbe0beb99225aa8282fdfc111be8
Shivanigupta2015 <guptashivani7409@gmail.com>
2025-01-25 09:51:13 -0500

    Add files via upload

---
+++ 1st program.py
@@ -0,0 +1,13 @@
+name = "Aashutosh"
+age = 22
+education = "M.com"
+print("Hellow Word")
+print("name")
+print(name)
+print(education)
+print("education")
+name = "Nitin"
+age = 24.0
+print(name)
+print(age)
+is_adult = True
---
+++ module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+print("printing Jokes...")
+#printing jokes this is ramdom jokes 
+"""
+so thansk 
+that was my problem 
+"""
